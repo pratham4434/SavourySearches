3b9e5502435eeb7b367e8a5e7ad778ade338da28
jradityamishra <106857292+jradityamishra@users.noreply.github.com>
2023-08-12 15:25:08 +0530

    reel front-end done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
  import {Contextlogin} from "./context/Contextlogin"
 import Home from './pages/Home';
+import Reel from './pages/Reel';
 function App() {
   return (
    <>
     <BrowserRouter>
     <Contextlogin.Provider>
     <Routes>
-      <Route path='/' element={<Home/>}/>
+      <Route path='/' element={<Reel/>}/>
     </Routes>
  </Contextlogin.Provider>
     </BrowserRouter>
